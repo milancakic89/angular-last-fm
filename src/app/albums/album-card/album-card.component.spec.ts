a9a3474518e98c7256fcdb46fde10d051e0ba050
Milan <milancakic2020@gmail.com>
2020-08-18 23:02:10 +0200

    Slightly refactored code

--- src/app/albums/album-card/album-card.component.spec.ts
+++ src/app/albums/album-card/album-card.component.spec.ts
@@ -2,6 +2,7 @@ import { AlbumCardComponent } from './album-card.component';
 import { AppService } from '../../app.service';
 import { Album } from '../../shared/album.model';
 import { ModalComponent } from '../../modal/modal.component';
+import { TestBed } from '@angular/core/testing';
 
 
 
@@ -11,10 +12,13 @@ describe('AlbumCardComponent', () => {
   let modal: ModalComponent;
 
   beforeEach(() => {
+    TestBed.configureTestingModule({
+      providers: [AppService]
+    })
     component = new AlbumCardComponent(null)
     service = new AppService(null);
     modal = new ModalComponent(null, null)
   })
-
+  // stoped here
 
 })
